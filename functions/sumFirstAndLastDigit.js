e2662a4991f8200ce458cc1cb2a336340d1ba24a
Petko Latev <Petkorlatev@gmail.com>
2024-01-27 10:51:08 +0200

    #new task

---
+++ functions/sumFirstAndLastDigit.js
@@ -0,0 +1,7 @@
+function sumFirstAndLastDigit(array) {
+    let first = array[0];
+    let last = array[array.length - 1]
+
+    console.log(first + last);
+}
+sumFirstAndLastDigit([10, 17, 22, 33])
