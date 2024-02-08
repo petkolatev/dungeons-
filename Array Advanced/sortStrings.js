e88e6740bd783e67e78fc3601714332a6c00f1cb
Petko Latev <Petkorlatev@gmail.com>
2024-02-09 00:49:36 +0200

    #new task

---
+++ Array Advanced/sortStrings.js
@@ -0,0 +1,11 @@
+function sortString(arr) {
+    let sortedByLetter = arr.sort()
+    let sortedByLength = arr.sort((a, b) => a.length - b.length)
+
+    for (const i of sortedByLength) {
+        console.log(i);
+    }
+
+}
+sortString(['alpha', 'beto', 'gamma', 'beta'])
+// sortString(['Isacc', 'Theodor', 'Jack', 'Harrison', 'George'])
