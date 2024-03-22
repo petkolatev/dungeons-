57a4f24d4de20630fcc332bce14666fbb5703149
Petko Latev <Petkorlatev@gmail.com>
2024-03-22 21:48:41 +0200

    #new task

--- RegEx/planetAttack.js
+++ RegEx/planetAttack.js
@@ -20,7 +20,7 @@ function planetAttack(input) {
         }
         command.push(message)
     }
-    let patternMove = /@(?<name>[A-Z][a-z]+)[^@\-!:>]*:(?<popul>\d+)[^@\-!:>]*!(?<action>[AD])![^@\-!:>]*->(?<attacker>\d+)/g
+    let patternMove = /@(?<name>[A-Z][a-z]+)([^@\-!:>])*:(?<popul>\d+)\2!(?<action>[AD])!\2->(?<attacker>\d+)/g
 
     let attacked = [];
     let destroyed = []
@@ -47,7 +47,6 @@ function planetAttack(input) {
         console.log(`-> ${des}`);
     }
 }
-planetAttack(['3',
-    "tt(''DGsvywgerx>6444444444%H%1B9444",
-    'GQhrr|A977777(H(TTTT',
+planetAttack(['2',
+    "STCDoghudd4=63333$D$0A53333",
     'EHfsytsnhf?8555&I&2C9555SR'])
